97aa5a1f7fbf2ab1bd66c6f819107a58aaee46f3
Srinivasa Durai <kaizel73@gmail.com>
2023-03-21 20:59:03 +0530

    final

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //SETTING CUSTOM MIDDLEWARE
 
 app.use((req, res, next) => {
-  let allowedOrigin = ["http://localhost:3000", "https://master--brilliant-malasada-8dfefd.netlify.app"]
+  let allowedOrigin = ["http://localhost:3000", "https://brilliant-malasada-8dfefd.netlify.app/"]
   if (allowedOrigin.indexOf(req.headers.origin) != -1) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.setHeader("Access-Control-Allow-Credentials", true);
